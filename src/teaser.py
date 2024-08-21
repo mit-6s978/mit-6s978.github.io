8f360f893413094464be61c944d6fa78e972e233
gmh14 <guomh2014@gmail.com>
2024-08-21 17:23:18 -0400

    u

--- src/teaser.py
+++ src/teaser.py
@@ -1,106 +1,100 @@
 import pyvista as pv
 from pyvista import examples
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
 
 res = 128
-step = 10
+step = 15
 
 data_pts = [
-    [10, 10, 10],
-    [118, 118, 118]    
+    [0, 0, 0],
+    [res - 1, res-1, res-1]    
 ]
-data_pts = np.array(data_pts)
+data_pts = np.array(data_pts, dtype=np.float64)
+
+sigmas = [0.45, 0.5]
+bld_w = [0.5, 0.5]
+
+def mixed_gaussian(x, y, z, data_pts, sigmas, bld_w):
+    x = x.unsqueeze(0)#.unsqueeze(-1).unsqueeze(-1)
+    y = y.unsqueeze(0)#.unsqueeze(-1).unsqueeze(0)
+    z = z.unsqueeze(0)#.unsqueeze(0).unsqueeze(-1)
+    
+    data_pts = torch.tensor(data_pts)#.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+    sigmas = torch.tensor(sigmas)#.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+    bld_w = torch.tensor(bld_w)#.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+    
+    diff_x = (x / res - data_pts[:, 0].view(-1, 1, 1, 1) / res)**2
+    diff_y = (y / res - data_pts[:, 1].view(-1, 1, 1, 1) / res)**2
+    diff_z = (z / res - data_pts[:, 2].view(-1, 1, 1, 1) / res)**2
+    dist_sq = diff_x + diff_y + diff_z
+
+    # Calculate the Gaussian values
+    gaussians = torch.exp(-dist_sq / (2 * sigmas.view(-1, 1, 1, 1)**2)) / (2 * np.pi * sigmas.view(-1, 1, 1, 1)**2)**(3/2)
+    
+    weighted_gaussians = bld_w.view(-1, 1, 1, 1) * gaussians
+
+    mixed = torch.sum(weighted_gaussians, dim=0)
+    return mixed
 
 # create a grid with gaussian value
-x = np.linspace(0, 128, res)
-y = np.linspace(0, 128, res)
-z = np.linspace(0, 128, res)
+x = np.linspace(0, res-1, res)
+y = np.linspace(0, res-1, res)
+z = np.linspace(0, res-1, res)
 x, y, z = np.meshgrid(x, y, z)
 
-# blending weights based on gaussian
-bld_w = []
-sigmas = [100, 100]
-for i, (px, py, pz) in enumerate(data_pts):
-    bld_w_i = np.exp(-0.5 * ((x - px)**2 + (y - py)**2 + (z - pz)**2) / sigmas[i] ** 2)
-    bld_w.append(bld_w_i)
-
-bld_w = np.stack(bld_w, axis=-1)
-bld_w /= np.sum(bld_w, axis=-1, keepdims=True)
-
 grid = pv.StructuredGrid(x, y, z)
-# Calculate the distance to the nearest data point
-paired_dist = [np.sqrt((x - px)**2 + (y - py)**2 + (z - pz)**2) for px, py, pz in data_pts]
-# distances = np.min(paired_dist, axis=0)
-distances = paired_dist[0] * bld_w[..., 0] + paired_dist[1] * bld_w[..., 1]
 
-# Normalize the distances to [0, 1] and invert for the coloring (darker when closer)
-distances_normalized = 1 - (distances / np.max(distances))
-distances_normalized = distances_normalized.flatten()
+x_param = torch.tensor(x, dtype=torch.float32, requires_grad=True)
+y_param = torch.tensor(y, dtype=torch.float32, requires_grad=True)
+z_param = torch.tensor(z, dtype=torch.float32, requires_grad=True)
+pdfs = mixed_gaussian(x_param, y_param, z_param, data_pts, sigmas, bld_w)
 
-# Assign the normalized distances as a scalar field to the grid
-grid['distance'] = distances_normalized
+pdfs_norm = pdfs.flatten().clone().detach().numpy()
 
-# Determine the closest data point index for each voxel
-closest_idx = np.argmin(paired_dist, axis=0)
+grid['pdfs_norm'] = pdfs_norm
 
-# Calculate the vectors pointing towards the closest data point
-vectors = np.zeros((res, res, res, 3))
-for i, (px, py, pz) in enumerate(data_pts):
-    mask = (closest_idx == i)
-    vectors[mask, 0] = px - x[mask]
-    vectors[mask, 2] = py - y[mask]
-    vectors[mask, 1] = pz - z[mask]
-    
-# vectors = np.stack([[px - x, pz - z, py - y] for px, py, pz in data_pts], axis=-1)
-# # vectors = np.stack([[px - x, py - y, pz - z] for px, py, pz in data_pts], axis=-1)
-# vectors = vectors * bld_w[None, ...]
-# vectors = np.sum(vectors, axis=-1).transpose(1, 2, 3, 0)
+log_pdfs = torch.log(pdfs)
+torch.sum(log_pdfs).backward()
+score = torch.stack([x_param.grad, y_param.grad, z_param.grad]).permute(1, 2, 3, 0)
+score = score.reshape(-1, 3)
+score = score.clone().detach().numpy()
 
-# Flatten the vectors for visualization
-vectors = vectors.reshape(-1, 3)
-vectors /= np.sum(bld_w, axis=-1).flatten()[:, None]
-vectors /= 25
-
-
-# Randomly pick up points for visualization
-# pts_idx = np.random.choice(res**3, 500, replace=False)
-# pts_idx = list(range(0, res**3, 3))
-x_subgrid = np.arange(0, res, step)
-y_subgrid = np.arange(0, res, step)
-z_subgrid = np.arange(0, res, step)
+x_subgrid = np.arange(step, res + 1, step)
+y_subgrid = np.arange(step, res + 1, step)
+z_subgrid = np.arange(step, res + 1, step)
 x_subgrid, y_subgrid, z_subgrid = np.meshgrid(x_subgrid, y_subgrid, z_subgrid)
 pts_idx = np.ravel_multi_index((x_subgrid.flatten(), y_subgrid.flatten(), z_subgrid.flatten()), (res, res, res))
+# pts_idx = np.random.choice(np.arange(res**3), 1000)
 
 points = grid.points[pts_idx]
-vectors_subgrid = vectors[pts_idx]
-
-# Create spheres at the data points
-sphere_radius = 3  # Adjust the size of the spheres as needed
-spheres = [pv.Sphere(radius=sphere_radius, center=pt) for pt in data_pts]
+vectors_subgrid = score[pts_idx]
 
 # visualize the voxel grid
 p = pv.Plotter()
-p.add_mesh(grid, scalars='distance', opacity=0.5, cmap='plasma')
-p.add_arrows(points, vectors_subgrid, mag=1.5, color='black')
+p.add_mesh(grid, scalars='pdfs_norm', opacity=0.4, cmap='plasma')
+# p.add_mesh(grid, scalars='pdfs_norm', opacity=0.5, cmap='inferno')
+# p.add_mesh(grid, scalars='pdfs_norm', opacity=0.5, cmap='viridis')
 
+# p.add_arrows(points, vectors_subgrid, mag=300.0, color='inferno')
 
-vectors = np.zeros((res, res, res, 3))
-for i, (px, py, pz) in enumerate(data_pts):
-    mask = (closest_idx != i)
-    vectors[mask, 0] = px - x[mask]
-    vectors[mask, 2] = py - y[mask]
-    vectors[mask, 1] = pz - z[mask]
-    
-# Flatten the vectors for visualization
-vectors = vectors.reshape(-1, 3)
-vectors /= np.sum(bld_w, axis=-1).flatten()[:, None]
-vectors /= 25
+colormap = plt.get_cmap('inferno')  
+colors = colormap(pdfs_norm)[:, :3][pts_idx]
+colors = np.ones_like(colors) * 255
 
-vectors_subgrid = vectors[pts_idx]
-p.add_arrows(points, vectors_subgrid, mag=1.5, color='red')
+arrow_mesh = pv.PolyData()
+arrow_mesh.points = points
+arrow_mesh["vectors"] = vectors_subgrid
+arrow_mesh["colors"] = colors
+arrows = arrow_mesh.glyph(orient="vectors", scale=False, factor=5.0)
+p.add_mesh(arrows, opacity=0.5, color='black')# scalars="colors" , cmap='plasma')
 
-# p.add_points(data_pts, color='red')
+sphere_radius = 3 
+spheres = [pv.Sphere(radius=sphere_radius, center=pt) for pt in data_pts]
 for sphere in spheres:
     p.add_mesh(sphere, color='red')
+
 p.show_axes()
 p.show()
